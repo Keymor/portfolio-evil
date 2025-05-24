c8432d3f6bd76eebb3ebabdae6a93ba950b21fcc
Keymor <vlad.kutuzov96@gmail.com>
2025-05-24 17:56:07 +0700

    evil added

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,17 @@ function App() {
           <Flame />
         </div>
       </div>
-      <div className='w-full h-[50svh] bg-[#efc210]'>
+      <div className='w-full h-[100svh] bg-[#efc210] flex flex-col sm:flex-row items-start'>
+        <div className='sm:translate-0 -translate-x-1/20 sm:ml-auto sm:mr-10 mx-auto size-auto z-10 relative hover:scale-110 duration-300 cursor-pointer'>
+          <img src='/devil.svg' className='size-fit sm:size-[20svw]' />
+          <img src='/eyes.svg' className='size-20 sm:size-[8svw] absolute top-1/10 right-2/8 blink delay-300' />
+          <h1 className=' absolute top-1/2 left-1/2 -translate-x-1/3 text-2xl font-bold whitespace-nowrap'>Project 1</h1>
+        </div>
+        <div className='sm:ml-10 sm:mr-auto mx-auto size-fit sm:size-[20svw] z-10 relative hover:scale-110 duration-300 cursor-pointer'>
+          <img src='/devil-1.svg' className='size-fit sm:size-[20svw]' />
+          <img src='/eyes.svg' className='size-20 sm:size-[8svw] absolute top-1/10 right-2/7 blinkTwo delay-1000' />
+          <h1 className=' absolute top-1/2 left-1/2 -translate-x-1/2 text-2xl font-bold whitespace-nowrap'>Project 2</h1>
+        </div>
       </div>
     </div>
   )
