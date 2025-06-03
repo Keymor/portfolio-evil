19731d79fad2e1c90c09519f72f2fe68818ac141
Keymor <vlad.kutuzov96@gmail.com>
2025-06-03 15:44:17 +0700

    position update

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
       <div className='w-fit self-center mx-auto bg-black text-white text-2xl px-15 py-3 rounded-4xl font-bold z-10'>MY WORK</div>
       <h1 className='w-fit text-4xl font-bold mx-auto text-shadow-md z-10 mt-5'>PROJECTS</h1>
       <div className='h-[50svh] flex flex-col w-screen sm:w-[1180px] sm:flex-row text-red-800 justify-center self-center gap-10 mt-10'>
-        <div onMouseEnter={() => setHover1(true)} onMouseLeave={() => setHover1(false)} className='sm:translate-0 -translate-x-1/20 z-1 relative hover:scale-120 duration-300 cursor-pointer drop-shadow-[0px_10px_10px_rgba(0,0,0,0.5)]'>
+        <div onMouseEnter={() => setHover1(true)} onMouseLeave={() => setHover1(false)} className='sm:translate-0 -translate-x-1/20 z-10 relative hover:scale-120 duration-300 cursor-pointer drop-shadow-[0px_10px_10px_rgba(0,0,0,0.5)]'>
           <div style={{ visibility: hover1 ? 'visible' : 'hidden', opacity: hover1 ? 1 : 0 }} className='bg-white rounded-2xl w-2/3 h-1/2 top-11/20 left-[55%] -translate-1/2 absolute inset-0 flex duration-300' />
           <img src='/devil.svg' className='size-fit sm:size-[27svw]' />
           <img style={{visibility: hover1 ? 'visible' : 'hidden'}} src='/eyes.svg' className='size-20 sm:size-[8svw] absolute top-1/10 right-[31%] blink delay-300' />
