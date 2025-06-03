8b3ef1d7ff855349f1dd096fcd1b9d5ee9296cc8
Keymor <vlad.kutuzov96@gmail.com>
2025-06-03 19:08:09 +0700

    mob UI update

--- src/App.tsx
+++ src/App.tsx
@@ -22,19 +22,19 @@ function App() {
     <div className='flex flex-col w-screen h-fit overflow-clip overflow-y-hidden fontAdd'>
       <Header />
       <div className='flex flex-col w-full h-[100svh] relative bg-black'>
-        <div className='flex flex-col sm:flex-row w-screen sm:w-[1180px] h-3/5 mt-[25%] sm:mt-[15%] justify-center self-center gap-[10vw]'>
-          <div style={{ animation: timer ? 'idle 1s ease-in-out infinite' : 'fall 1.5s ease-in-out' }} className='rounded-2xl flex flex-col gap-1 z-1'>
+        <div className='flex flex-col sm:flex-row w-full sm:w-[1180px] h-3/5 mt-[35%] sm:mt-[15%] justify-center self-center gap-[10vw]'>
+          <div style={{ animation: timer ? 'idle 1s ease-in-out infinite' : 'fall 1.5s ease-in-out' }} className='rounded-2xl flex flex-col gap-1 z-1 w-full p-[5vw]'>
             <h1 className='py-3 px-4 font-bold text-xl bg-[#cb3838] rounded-4xl text-center w-fit text-white'>FULL STACK WEB DEVELOPER</h1>
             <img src='/HEY.svg' className='h-fit w-4/10 my-1' />
-            <h1 className='text-5xl font-bold text-white whitespace-nowrap'>I'M VLADYSLAV KUTUZOV</h1>
-            <h1 className='text-5xl font-bold text-white whitespace-nowrap'>I BUILD FRIENDLY WEB EXPERIENCES</h1>
-            <div className='flex flex-row gap-10 mt-6'>
+            <h1 className='text-3xl sm:text-5xl font-bold text-white whitespace-nowrap'>I'M VLADYSLAV KUTUZOV</h1>
+            <h1 className='text-3xl sm:text-5xl font-bold text-white whitespace-nowrap'>I BUILD FRIENDLY WEB EXPERIENCES</h1>
+            <div className='flex flex-row gap-5 sm:gap-10 mt-2 sm:mt-6'>
               <div className='size-15 bg-[#cb3838] rounded-full' />
               <div className='size-15 bg-[#cb3838] rounded-full' />
               <div className='size-15 bg-[#cb3838] rounded-full' />
             </div>
           </div>
-          <img style={{ animation: timer ? 'evil 1s ease-in-out infinite' : 'appearence 1.5s ease-in-out forwards' }} src='/demon.svg' className='z-1 size-100 sm:size-130 translate-x-1/10' />
+          <img style={{ animation: timer ? 'evil 1s ease-in-out infinite' : 'appearence 1.5s ease-in-out forwards' }} src='/demon.svg' className='z-1 size-80 mx-auto sm:mx-0 sm:size-130 translate-x-1/10' />
         </div>
         <Fire color='rgb(150, 25, 25' amount={50} />
         <div className='w-full -mb-140 mt-auto scale-x-110 z-0'>
@@ -50,15 +50,15 @@ function App() {
       <div className='h-[50svh] flex flex-col w-screen sm:w-[1180px] sm:flex-row text-red-800 justify-center self-center gap-10 mt-10'>
         <div onMouseEnter={() => setHover1(true)} onMouseLeave={() => setHover1(false)} className='sm:translate-0 -translate-x-1/20 z-10 relative hover:scale-120 duration-300 cursor-pointer drop-shadow-[0px_10px_10px_rgba(0,0,0,0.5)]'>
           <div style={{ visibility: hover1 ? 'visible' : 'hidden', opacity: hover1 ? 1 : 0 }} className='bg-white rounded-2xl w-2/3 h-1/2 top-11/20 left-[55%] -translate-1/2 absolute inset-0 flex duration-300' />
-          <img src='/devil.svg' className='size-fit sm:size-[27svw]' />
+          <img src='/devil.svg' className='mx-auto sm:mx-0 size-fit sm:size-[27svw]' />
           <img style={{visibility: hover1 ? 'visible' : 'hidden'}} src='/eyes.svg' className='size-20 sm:size-[8svw] absolute top-1/10 right-[31%] blink delay-300' />
-          <h1 style={{ visibility: hover1 ? 'hidden' : 'visible'}} className='absolute left-11/20 top-1/2 -translate-1/2 text-7xl font-bold whitespace-nowrap'>PROJECT 1</h1>
+          <h1 style={{ visibility: hover1 ? 'hidden' : 'visible'}} className='absolute left-11/20 top-1/2 -translate-1/2 sm:-translate-1/2 text-5xl sm:text-7xl font-bold whitespace-nowrap'>PROJECT 1</h1>
         </div>
         <div onMouseEnter={() => setHover2(true)} onMouseLeave={() => setHover2(false)} className='z-10 relative hover:scale-120 duration-300 cursor-pointer drop-shadow-[0px_10px_10px_rgba(0,0,0,0.5)]'>
           <div style={{ visibility: hover2 ? 'visible' : 'hidden', opacity: hover2 ? 1 : 0 }} className='bg-white rounded-2xl w-2/3 h-1/2 top-11/20 left-[49.3%] -translate-1/2 absolute inset-0 flex duration-300' />
-          <img src='/devil-1.svg' className='size-fit sm:size-[28.5svw]' />
+          <img src='/devil-1.svg' className='mx-auto sm:mx-0 size-fit sm:size-[28.5svw]' />
           <img style={{visibility: hover2 ? 'visible' : 'hidden'}} src='/eyes.svg' className='size-20 sm:size-[8svw] absolute top-1/10 right-3/8 blinkTwo delay-1000' />
-          <h1 style={{ visibility: hover2 ? 'hidden' : 'visible'}} className=' absolute top-1/2 left-1/2 -translate-1/2 text-7xl font-bold whitespace-nowrap'>PROJECT 2</h1>
+          <h1 style={{ visibility: hover2 ? 'hidden' : 'visible'}} className=' absolute top-1/2 left-1/2 -translate-1/2 text-5xl sm:text-7xl font-bold whitespace-nowrap'>PROJECT 2</h1>
         </div>
       </div>
       <div className='h-[15svh]' />
--- src/components/contacts.tsx
+++ src/components/contacts.tsx
@@ -3,12 +3,12 @@ import Fire from "./particless"
 export default function Contact() {
     return (
         <div className="h-screen w-screen flex relative">
-            <img src="/mauntings_back.svg" className="absolute inset-0 w-full top-auto -bottom-80 z-1"/>
-            <img src="/front_mauntain.svg" className="absolute inset-0 w-full top-auto -bottom-80 z-0"/>
+            <img src="/mauntings_back.svg" className="absolute inset-0 w-full top-auto scale-200 sm:scale-100 sm:-bottom-80 z-1"/>
+            <img src="/front_mauntain.svg" className="absolute inset-0 w-full top-auto scale-200 sm:scale-100 sm:-bottom-80 z-0"/>
             <Fire color="rgb(114, 114, 114" amount={20}/>
-            <div className="size-fit m-auto mt-[10%] text-center flex flex-col gap-5 z-1">
+            <div className="w-9/10 h-1/2 sm:size-fit m-auto mt-[40%] sm:mt-[10%] text-center flex flex-col gap-5 z-1">
                 <h1 className="text-white font-bold text-3xl text-shadow-md">CONTACT</h1>
-                <div className="h-[32svh] w-[34svw] gap-5 p-10 min-h-fit bg-black rounded-4xl flex flex-col">
+                <div className="sm:h-[32svh] sm:w-[34svw] h-full w-full gap-5 p-10 min-h-fit bg-black rounded-4xl flex flex-col">
                     <div className="px-[15%] gap-[18%] w-full min-h-fit h-1/4 flex flex-row mx-auto mt-auto cursor-pointer">
                         <div className="size-20 rounded-full bg-[#efb010] my-auto"/>
                         <h1 className="my-auto font-bold text-2xl text-[#efb010]">GMAIL</h1>
